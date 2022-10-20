c9a37f475e2469da0cfc70d1ccd4a481900c85f2
m1siraj <50576588+m1siraj@users.noreply.github.com>
2022-10-19 21:45:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import pandas as pd
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
 df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 # print(df.head(5))
